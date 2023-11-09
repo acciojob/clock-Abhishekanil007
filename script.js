60b3fbbb330ada0c9919e3e9ad358b0a3e9dc75e
Abhishek Anil <141896050+Abhishekanil007@users.noreply.github.com>
2023-11-09 15:04:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function updateDateTime(){
+	const now = new Date();
+	const currentTimeDate = now.toLocaleString();
+	document.getElementById('timer').textContent = currentTimeDate;
+	
+}
+
+setInterval(updateDateTime, 1000);
